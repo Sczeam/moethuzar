12648725d734198aa186aacdbbb676bc41ddd5c7
Sczeam <zawhtikeaung19@gmail.com>
2026-03-18 22:09:51 +0000

    Replace account login page with register journey

--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -1,5 +1,5 @@
-import AccountLoginForm from "./login-form";
 import { sanitizeNextPath } from "@/server/auth/redirect";
+import RegisterJourney from "@/app/account/register/register-journey";
 
 export default async function AccountLoginPage({
   searchParams,
@@ -10,8 +10,8 @@ export default async function AccountLoginPage({
   const nextPath = sanitizeNextPath(params.next, "/account");
 
   return (
-    <main className="mx-auto flex min-h-[70vh] max-w-md items-center px-4 py-10">
-      <AccountLoginForm nextPath={nextPath} />
+    <main className="mx-auto flex min-h-[70vh] max-w-6xl items-center px-4 py-10 sm:px-6 lg:px-8">
+      <RegisterJourney nextPath={nextPath} />
     </main>
   );
 }
